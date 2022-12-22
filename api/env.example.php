d6cd97c807f017d62876ba6cee64b2c93313026a
eduardo ribeiro <edu0995@gmail.com>
2022-12-22 12:44:42 -0300

    removing env example

--- api/env.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-putenv('MYSQL_HOST=127.0.0.1');
-putenv('MYSQL_DBNAME=betsson_api');
-putenv('MYSQL_USER=');
-putenv('MYSQL_PASSWORD=');
-putenv('MYSQL_PORT=');
---
+++ api/env.php
@@ -0,0 +1,7 @@
+<?php
+
+putenv('MYSQL_HOST=betsson-db');
+putenv('MYSQL_DBNAME=betsson_api');
+putenv('MYSQL_USER=root');
+putenv('MYSQL_PASSWORD=root');
+putenv('MYSQL_PORT=3306');
